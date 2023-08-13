3281b1b29110cd51c5415f4397419991a57e9870
ganupadval <ganesh26padval@gmail.com>
2023-08-13 14:13:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container text-center mw-100 m-0 ">
+        <h2>Title</h2>
+      </div>
+      <div className="container text-center mw-100 ">
+        <div className="row m-auto " style={{ height: "92vh" }}>
+          <div className="col-3 bg-primary d-none d-md-block">1 of 2</div>
+          <div className="col-md-9 bg-secondary ">
+            <div className="m-auto mt-2">
+              <div className="bg-primary " style={{ height: "80vh" }}>
+                a
+              </div>
+              <div className="bg-success mt-2" style={{ height: "8vh", display:'flex' }}>
+                <div className="m-auto">b</div>
+                <div className="bg-secondary mt-1" style={{height:"45px", width:"45px", marginRight:'10px'}}>c</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
