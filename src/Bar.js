6b3a305ee8a366965d534fddd69b1c42655a5ca0
ganupadval <ganesh26padval@gmail.com>
2023-08-18 19:13:02 +0530

    updated css

--- src/Bar.js
+++ src/Bar.js
@@ -1,32 +1,10 @@
-import React, { useState, useEffect } from "react";
-import ProgressBar from "./progressbar";
-
-
-const time = 30;
+import React from 'react';
+import './App.css'; // Import the CSS file
 
 export default function Bar() {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    let seconds = 0;
-    const interval = setInterval(() => {
-      if (time >= seconds) {
-        const newProgress = (seconds / time) * 100;
-        setProgress(newProgress);
-        seconds += 1;
-      } else {
-        clearInterval(interval); // Stop the interval when time is up
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(interval); // Clean up the interval on unmount
-    };
-  }, [time]);
-
   return (
-    <div className="App">
-      <ProgressBar progress={progress} />
+    <div className="progressBar mt-1">
+      <div className="progress"></div>
     </div>
   );
 }
